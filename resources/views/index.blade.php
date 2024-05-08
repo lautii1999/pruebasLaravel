be2af42aa3d607b5e7210f753fac2c0af16b008f
lautii1999 <lautarogarzonmega@gmail.com>
2024-05-08 00:54:11 -0300

    primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Monchi gato</title>
+</head>
+<body>
+    <h1>el proyecto que no sabemos que es</h1>
+    <p>adjaklsdjalksdjaslkdjaskldjaklsdj</p>
+    
+</body>
+</html>
